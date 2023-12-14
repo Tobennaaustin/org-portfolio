4dbe47e4f240359e0daad96d525bf3249661f263
tobenna <tobennaaustin@gmail.com>
2023-12-14 10:13:59 -0100

    first commit

---
+++ js/valid-message.js
@@ -0,0 +1,10 @@
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+
+
+function submit(){
+    if (name === ''){
+        name.style.boder = "2px red"
+    }
+}
+
