c3ee744d101620f98a148b9bf866db0a12b45331
tobenna <tobennaaustin@gmail.com>
2023-12-28 11:23:39 -0100

    made some changes

--- js/main.js
+++ js/main.js
@@ -19,3 +19,30 @@ function showHide(){
 
 openNav.onclick = showHide;
 closeNav.onclick = showHide;
+
+
+// scroll to top code
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+mybutton.addEventListener('click', () =>{
+    topFunction();
+});
